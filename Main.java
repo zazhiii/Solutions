46e1d9326e535519b2507eadba4d40da9da3adc8
nuclear <2065707071@qq.com>
2024-09-09 20:40:38 +0800

    refactor: 保留输入输出模板、其他模板分离出去

--- Main.java
+++ Main.java
@@ -17,60 +17,18 @@ public class Main {
     static Read rd = new Read();
     static PrintWriter pw = new PrintWriter(System.out);
     static int inf = (int)2e9;
-    static long INF = (long)2e18;
-
-    /* 第一个大于等于t的值的位置 */
-    public static int lower_bound(int a[], int l, int r, int t){
-        int ans = r + 1;
-        while(l <= r){
-            int m = (l + r) >>> 1;
-            if(a[m] >= t){ans = m; r = m - 1;}
-            else l = m + 1;
-        }
-        return ans;
-    }
-    /* 第一个大于t的值的位置 */
-    public static int upper_bound(int a[], int l, int r, int t){
-        int ans = r + 1;
-        while(l <= r){
-            int m = (l + r) >>> 1;
-            if(a[m] > t){ans = m; r = m - 1;}
-            else l = m + 1;
-        }
-        return ans;
-    }
-    /* 下一个排列 */
-    public static boolean nextPermutation(int a[]){
-        int n = a.length, i = n - 2;
-        while(i >= 0 && a[i] > a[i + 1]) i --;
-        if(i < 0) return false;
-        int k = i + 1;
-        while(k < n && a[k] > a[i]) k ++;
-        {int t = a[i]; a[i] = a[k - 1]; a[k - 1] = t;}// swap(a[i], a[k - 1])
-        Arrays.sort(a, i + 1, n);
-        return true;
-    }
-    
+    static long INF = (long)2e18; 
 }
 
 class Read{
     StringTokenizer st;
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     String next(){
-        while (st == null || !st.hasMoreElements()){
-            try {st = new StringTokenizer(br.readLine());}
-            catch (IOException e){e.printStackTrace();}
-        }
+        while (st == null || !st.hasMoreElements())st = new StringTokenizer(br.readLine());
         return st.nextToken();
     }
     int nextInt() {return Integer.parseInt(next());}
     long nextLong() {return Long.parseLong(next());}
     double nextDouble() {return Double.parseDouble(next());}
-    String nextLine() {
-        String str = "";
-        try {str = br.readLine();} 
-        catch (IOException e) {e.printStackTrace();}
-        return str;
-    }
-
+    String nextLine() {return br.readLine();}
 }
